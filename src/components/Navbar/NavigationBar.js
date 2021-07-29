0ad340eee337fe71d153a20ad3012dfe6868c775
unknown <snehdeepsingh221199@gmail.com>
2021-07-29 22:14:48 +0530

    mod homepage

--- src/components/Navbar/NavigationBar.js
+++ src/components/Navbar/NavigationBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import './NavigationBar.css';
 
@@ -7,10 +8,10 @@ function NavigationBar() {
         <div className="nav-container">
             <Navbar fixed="top" bg="light" variant="light">
                 <Container styles={{width: "100%"}}>
-                    <Navbar.Brand href="/" className="brand">GoSolid</Navbar.Brand>
+                    <Navbar.Brand className="brand"><Link to='/'>GoSolid</Link></Navbar.Brand>
                     <Nav className="me-auto links">
-                        <Nav.Link href="product-page" style={{fontWeight: 'bold'}}>Products</Nav.Link>
-                        <Nav.Link href="cart" style={{fontWeight: 'bold'}}>Cart</Nav.Link>
+                        <Nav.Link style={{fontWeight: 'bold'}}><Link to='/product-page'>Products</Link></Nav.Link>
+                        <Nav.Link style={{fontWeight: 'bold'}}><Link to='/cart'>Cart</Link></Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>
